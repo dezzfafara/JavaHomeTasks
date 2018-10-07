0dff3b6c5c60d9668d78e2dad8e6845a9b55f2d4
Slava <dezzfafara@gmail.com>
2018-10-07 19:34:06 +0300

    Add HomeTask1

---
+++ HomeTask1/Task1_2 - reverse.java
@@ -0,0 +1,10 @@
+package task1_2;
+
+public class Task1_2 {
+	public static void main(String[] args) {
+		for (int i = args.length - 1; i >= 0; i--) {
+			System.out.println("Argument " + (i + 1) + " = " + args[i]);
+		}
+	}
+
+}
---
+++ HomeTask1/Task1_6.java
@@ -0,0 +1,21 @@
+package task1_6;
+
+public class Task1_6 {
+	public static void main(String[] args) {
+
+		byte b = 115;
+		int i = 2;
+		int c = 10;
+		int c1 = 0777;
+		int c2 = 0x32;
+		int c3 = 0b10;
+		short s = 12;
+		double d = 999999.25;
+		float f = 99.25f;
+		char e = 'x';
+		boolean t = true;
+		System.out.println(b + " " + i + " " + c + " " + c1 + " " + c2 + " " + c3 + " " + s + " " + d + " " + f + " "
+				+ e + " " + t);
+	}
+
+}
