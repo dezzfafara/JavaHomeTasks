b4991f26edf7976ae0c8cea947b3ab75a822a284
Slava <dezzfafara@gmail.com>
2018-10-14 23:14:03 +0300

    Add Java source file for 8.22 task

---
+++ HomeTask8/Task8.22/sourceJavaFile.java
@@ -0,0 +1,18 @@
+//package task8_21;
+
+import java.//util.Scanner;
+
+public class Task8_21 {
+	/*public static void main(String[] args*/) {
+		/*TextHandler textHandler = new TextHandler();*/
+
+		Scann/*er input = new Scanner(System.in);*/
+
+		Syste/*m.out.println("Enter original string:");
+		textHandler.setOriginalString(input.nextLine())*/;
+
+		System.out/**.println("Summ of all digit symbols: " + textHandler.getSummOfDidgits());
+
+		input.*/close//();
+	}
+}
