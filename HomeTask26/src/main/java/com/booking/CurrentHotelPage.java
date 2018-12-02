1ed0caf9fd3ed049aef51e342b2c4bca72c8dfc7
Slava <dezzfafara@gmail.com>
2018-12-02 20:42:25 +0300

    Add HomeTask26

---
+++ HomeTask26/src/main/java/com/booking/CurrentHotelPage.java
@@ -0,0 +1,22 @@
+package com.booking;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CurrentHotelPage {
+
+	@FindBy(xpath = ".//div[@class='bui-review-score c-score bui-review-score--end']"
+			+ "//div[@class='bui-review-score__badge']")
+	private WebElement reviewScore;
+
+	public CurrentHotelPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public double getParsedReviewScore() {
+		return Double.parseDouble(reviewScore.getText().trim());
+	}
+
+}
